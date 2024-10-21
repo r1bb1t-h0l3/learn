61a00b6ec856492dd45b99cc049f8b13fe5291b8
sasha <arefieva.aleksandra@gmail.com>
2024-10-21 21:35:41 +0200

    added Timus challenge Action Figures

--- dmoj2.py
+++ dmoj2.py
@@ -152,3 +152,208 @@
 # print(f'Player B: {score_b} point(s).')
 
 #55 Action Figures https://acm.timus.ru/problem.aspx?space=1&num=2144
+
+#Read input
+#Check whether all boxes are OK
+#Obtain a new lit of boxees with only left and right heights
+#Sort boxes
+#Determine whether boxes are organised
+
+
+
+# def read_boxes(n):
+#     """
+#     n is the number of boxes to read.
+
+#     Read the boxes from the input, and return them as a list of boxes;
+#     Each box is a list of action figure heights.
+#     """
+
+#     boxes = []
+#     for i in range(n):
+#         box = input().split()
+#         box.pop(0)
+#         for i in range(len(box)):
+#             box[i] = int(box[i])
+#         boxes.append(box)
+#     return boxes
+
+# def all_boxes_ok(boxes):
+
+def read_boxes(n):
+    """
+    n is the number of boxes to read.
+
+    Read the boxes from the input, and return them as a list of boxes;
+    Each box is a list of action figure heights.
+    """
+
+    boxes = []
+    for i in range(n):
+        box = input().split()
+        box.pop(0)
+        for i in range(len(box)):
+            box[i] = int(box[i])
+        boxes.append(box)
+    return boxes
+
+def all_boxes_ok(boxes):
+    """
+    boxes is a list of boxes; each box is a list of action figure heights.
+
+    Return True if each box in boxes has its action figures in 
+    nondecreasing order of height, False otherwise.
+    """
+
+    for box in boxes:
+        if not box_ok(box):
+            return False
+        return True
+
+def box_ok(box):
+    """
+    box is the list of action figure heights in a given box.
+
+    Return True if the heights in box are in nondecreasing order,
+    False otherwise.
+    """
+
+    for i in range(len(box) - 1):
+        if box[i] > box[i + 1]:
+            return False
+
+    return True
+
+def boxes_endpoints(boxes):
+    """
+    boxes is a list of boxes; each box is a lit of action figure heights.
+
+    return a list, where each value is a list of two values:
+    the heights of the leftmost and rightmost figures in a box.
+    """
+
+    endpoints = []
+    for box in boxes:
+        endpoints.append(box[0], box[-1])
+    return endpoints
+
+def all_endpoints_ok(endpoints):
+    """
+    endpoints is a list, where each value is a list of two values:
+    the heights of the leftmost and rightmost action figures in a box.
+
+    Requires: endponts is sorted by action figure heights.
+
+    Return True if the endpoints came from boxes that can be put in order,
+    False otherwise.
+    """
+
+    maximum = endpoints[0][1]
+    for i in range(1, len(endpoints)):
+        if endpoints[i][0] < maximum:
+            return False
+        maximum = endpoints[i][1]
+    return True
+
+#Main Program
+#Read input
+n = int(input())
+boxes = read_boxes(n)
+
+#Check whether all boxes are OK
+if not all_boxes_ok(boxes):
+    print('NO')
+else:
+    #Obtain a new list of boxes with only left and ight heights
+    endpoints = boxes_endpoints(boxes)
+
+    #Sort boxes
+    endpoints.sort()
+
+    #Determine whether boxes are organised
+    if all_endpoints_ok(endpoints):
+        print('YES')
+    else:
+        print('NO')
+
+
+#     """
+#     boxes is a list of boxes; each box is a list of action figure heights.
+
+#     Return True if each box in boxes has its action figures in 
+#     nondecreasing order of height, False otherwise.
+#     """
+
+#     for box in boxes:
+#         if not box_ok(box):
+#             return False
+#         return True
+
+# def box_ok(box):
+#     """
+#     box is the list of action figure heights in a given box.
+
+#     Return True if the heights in box are in nondecreasing order,
+#     False otherwise.
+#     """
+
+#     for i in range(len(box) - 1):
+#         if box[i] > box[i + 1]:
+#             return False
+
+#     return True
+
+# def boxes_endpoints(boxes):
+#     """
+#     boxes is a list of boxes; each box is a lit of action figure heights.
+
+#     return a list, where each value is a list of two values:
+#     the heights of the leftmost and rightmost figures in a box.
+#     """
+
+#     endpoints = []
+#     for box in boxes:
+#         endpoints.append(box[0], box[-1])
+#     return endpoints
+
+# def all_endpoints_ok(endpoints):
+#     """
+#     endpoints is a list, where each value is a list of two values:
+#     the heights of the leftmost and rightmost action figures in a box.
+
+#     Requires: endponts is sorted by action figure heights.
+
+#     Return True if the endpoints came from boxes that can be put in order,
+#     False otherwise.
+#     """
+
+#     maximum = endpoints[0][1]
+#     for i in range(1, len(endpoints)):
+#         if endpoints[i][0] < maximum:
+#             return False
+#         maximum = endpoints[i][1]
+#     return True
+
+# #Main Program
+# #Read input
+# n = int(input())
+# boxes = read_boxes(n)
+
+# #Check whether all boxes are OK
+# if not all_boxes_ok(boxes):
+#     print('NO')
+# else:
+#     #Obtain a new list of boxes with only left and ight heights
+#     endpoints = boxes_endpoints(boxes)
+
+#     #Sort boxes
+#     endpoints.sort()
+
+#     #Determine whether boxes are organised
+#     if all_endpoints_ok(endpoints):
+#         print('YES')
+#     else:
+#         print('NO')
+
+
+
